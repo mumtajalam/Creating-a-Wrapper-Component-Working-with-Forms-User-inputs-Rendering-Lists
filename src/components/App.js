a3886bb7a85a85ddae2c02cfb095a726290b2e5d
Md Mumtaj Alam <98735031+mumtajalam@users.noreply.github.com>
2022-06-12 20:53:09 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,28 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-    render() {
-
-        return(
-            <div id="main">
-               {/* Do not remove the main div */}
-            </div>
-        )
-    }
+  constructor(props) {
+    super(props);
+    this.relatives = [
+      { name: "Mumtaj" },
+      { name: "Ameen" },
+      { name: "Amir" },
+      { name: "subhradip" }
+    ];
+  }
+  render() {
+    return (
+      <div id="main">
+        <ol key={"relativeList"}>
+          {this.relatives &&
+            this.relatives.map((item, index) => (
+              <li key={`relativeListItem${index + 1}`}>{item.name}</li>
+            ))}
+        </ol>
+      </div>
+    );
+  }
 }
-
 
 export default App;
